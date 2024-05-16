bdfd22b8b86aa10cd96db7b19a6cd5fed003f114
haftom abrha <haftomabhra452@gmail.com>
2024-05-16 09:19:59 +0300

    adding last name

--- demo/src/main/java/com/example/demo/model/Student.java
+++ demo/src/main/java/com/example/demo/model/Student.java
@@ -14,10 +14,15 @@ public class Student {
     private int id;
     private String name;
     private String address;
+    private String lastName;
+    
 
     public Student() {
     }
+  public void setLastName(String lname){
+    lastName=lname;
 
+  }
     public void setId(int id) {
         this.id = id;
     }
